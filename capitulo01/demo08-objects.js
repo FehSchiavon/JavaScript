9d893814bd14f83182432cb60b910e8929ce2993
FehSchiavon <felipschiavon@gmail.com>
2020-05-26 08:18:07 -0300

    core: Aprendendo a usar melhor Objetos

---
+++ capitulo01/demo08-objects.js
@@ -0,0 +1,31 @@
+const heroi = {
+    nome: 'Flash',
+    idade: 100,
+    sexo: 'Masculino'
+}
+
+// console.log('nome', heroi.nome)
+// console.log('idade', heroi['idade'])
+// console.log('sexo', heroi.sexo)
+// console.log('naoExiste', heroi.naoExiste)
+
+// ADICIONA mais Itens no Objeto
+// heroi.id = 001
+// console.log(heroi)
+
+// Pegar as CHAVES (KEYS) dos Objetos OBJECTS.KEYS
+// console.log(Object.keys(heroi))
+
+// Pegar os VALORES (VALUES) dos Objetos OBJECTS.VALUES
+// console.log(Object.values(heroi))
+
+// JUNTAR dois OBJECTOS ASSIGN
+// const pessoa = {
+//     tamanho: '10 metros'
+// }
+// const novoObj = Object.assign(heroi, pessoa)
+// console.log(novoObj)
+
+// DELETAR um Objecto
+delete novoObj.nome
+console.log(novoObj)
